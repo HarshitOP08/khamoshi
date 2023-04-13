27e47384bec7b4c2cca59fd1142fd71312cede65
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-04-13 11:28:51 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "5288547197" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "YOUR_GODFATHER_XD"
+    OWNER_ID = "1952625698" # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "I_LOVE_YOU_MY_HEARTBEET"
     SUPPORT_CHAT = "INCRICIBLE"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001940575506
+       -1001770300514 
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001940575506
+        -1001770300514
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
     # RECOMMENDED
